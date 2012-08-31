d09d0660e209811d8f8cde26874807354c92d8dc
Corey Chivers <corey.chivers@mail.mcgill.ca>
2012-08-31 12:41:28 -0400

    write to png

--- POE_curves.R
+++ POE_curves.R
@@ -5,18 +5,22 @@
 
 c_pars<-seq(0,2,length.out=11)
 
-curve(1-exp(-x),
-   xlab='Propagule Pressure',
-   ylab='Probability of establishment',
-   xlim=c(0,5),
-   lwd=1.5)
 
-for(i in 2:11)
-   curve(1-exp(-x^c_pars[i]),
-      add=T,
-      lty=i,
-      col=i,
+png('POE_curves.png')
+
+   curve(1-exp(-x),
+      xlab='Propagule Pressure',
+      ylab='Probability of establishment',
+      xlim=c(0,5),
       lwd=1.5)
 
-legend('bottomright',legend=paste("c =",c_pars[2:11]),lty=2:11,col=2:11,lwd=1.5)
+   for(i in 2:11)
+      curve(1-exp(-x^c_pars[i]),
+         add=T,
+         lty=i,
+         col=i,
+         lwd=1.5)
 
+   legend('bottomright',legend=paste("c =",c_pars[2:11]),lty=2:11,col=2:11,lwd=1.5)
+
+dev.off()
